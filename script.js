8a73e2ee25c0ecc881e99ba506154cb330b73cd3
sjp261019 <sjp261019@gmail.com>
2022-01-30 14:20:37 -0500

    01/30/22 - 2

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
-var xpos = [];
-var ypos = [];
-var r = [];
-var g = [];
-var b = [];
-var a = [];
-var s = [];
 
-function setup() {
-  let cnv = createCanvas(500, 500);
-  cnv.parent('container');
-  background(220);
-}
+$(document).ready(function(){
+  $(".menu3").children().draggable();
 
-function cir(){
-  xpos.push(random(windowWidth));
-  ypos.push(random(windowHeight));
-  r.push(random(255));
-  g.push(random(255));
-  b.push(random(255));
-  a.push(random(100));
-  s.push(random(400));
-}
+  $("#menuitem1").click(function(){
+        //console.log("click");
+        $("#container1").html("<p>This is a paragraph</p>");
+  });
 
-function draw() {
-  for (i=0; i<=50; i++){
-    noStroke();
-    ellipse(xpos[i], ypos[i], s[i]);
-    fill(r[i], g[i], b[i], a[i]);
-  }
-  cir();
-}
+  $("#menuitem2").click(function(){
+    $("#container1").text("This is the text method");
+  });
+  
+  $("#menuitem3").click(function(){
+    $("#container1").append("This is the append method");
+  });
+
+  $(".menu2").children().hover(function (){
+    //console.log("children!");
+    $(this).toggleClass("menuitems");
+    $(this).toggleClass("menuitems2");
+    //$("#container2").slideToggle();
+    //$("#container2").fadeToggle();
+//     $("#container2").animate({
+//         opacity:"0.2",
+//         height: "60%",
+//         width: "60%"
+//     }, 2000, function(){
+//         alert("The paragraph is now hidden");
+//     });
+    });
+
+    $("#menuitem6").click(function(){
+        console.log("menuitem6 clicked");
+        scrollTo(0, 5000);
+    });
+    
+});
+
+    // $("#menuitem1").click(function(){
+    //       $("#container1").html("<p>hello this is an html<br>paragraph element</p>");
+    //   })
+
+    // $("#menuitem2").click(function(){
+    //     $("#container1").append("this is append");
+    // })
+
+    // $("#menuitem3").click(function(){
+    //   $("#container1").text("this is text");
+    // })
+
+    //   $(".menu2").children().click(function(){
+    //     console.log("click")
+    //     $(this).toggleClass("menuitems")
+    //     $(this).toggleClass("menuitems2")
+    //     console.log("click")
+    //     $("#container2").slideToggle(1000, "swing");
+    //     $("#container2").fadeToggle();
+    //     $("#container2").animate({
+    //       opacity: '0.2',
+    //       height: '60%',
+    //       width: '60%',},2000,function(){
+    //         alert("The paragraph is now hidden");
+    //     });
+    //     })
+    //   $("#menuitem6").click(function(){
+    //       scrollTo(0,5000)
+    //   })
+
+    //   $( ".menu3" ).children().draggable();
+
