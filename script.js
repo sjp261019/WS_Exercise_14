ff3d7daf32173a05e872f6a518db8dffab9e11da
sjp261019 <sjp261019@gmail.com>
2022-01-31 08:23:47 -0500

    01/31/22

--- script.js
+++ script.js
@@ -1,71 +1,42 @@
-
 $(document).ready(function(){
-  $(".menu3").children().draggable();
-
-  $("#menuitem1").click(function(){
-        //console.log("click");
-        $("#container1").html("<p>This is a paragraph</p>");
-  });
-
-  $("#menuitem2").click(function(){
-    $("#container1").text("This is the text method");
-  });
   
-  $("#menuitem3").click(function(){
-    $("#container1").append("This is the append method");
+  $.fn.toggleText = function(t1, t2){
+    if (this.text() == t1) this.text(t2);
+    else                   this.text(t1);
+    return this;
+  };
+
+  $(".menu").children().hover(function (){
+    $(this).toggleClass("menuitems2");
+    $(this).toggleText('hello', '');
   });
 
-  $(".menu2").children().hover(function (){
-    //console.log("children!");
-    $(this).toggleClass("menuitems");
-    $(this).toggleClass("menuitems2");
-    //$("#container2").slideToggle();
-    //$("#container2").fadeToggle();
-//     $("#container2").animate({
-//         opacity:"0.2",
-//         height: "60%",
-//         width: "60%"
-//     }, 2000, function(){
-//         alert("The paragraph is now hidden");
-//     });
-    });
-
-    $("#menuitem6").click(function(){
-        console.log("menuitem6 clicked");
-        scrollTo(0, 5000);
-    });
+  $(document).ready(function(){
     
+    $(".menu2").children().hover(function (){
+      $(this).toggleClass("menuitems2");
+      $(this).toggleText('hello', '');
+    });
+  });
+
+
+  $(document).ready(function(){
+    
+    $(".menu3").children().hover(function (){
+      $(this).toggleClass("menuitems2");
+      $(this).toggleText('hello', '');
+    });
+  });
+
+  $(document).mousemove(function(e){
+    $("#circle").css({
+      left: e.pageX,
+      top: e.pageY
+    });
+  });
+
+  document.body.style.cursor = "none";
+
 });
 
-    // $("#menuitem1").click(function(){
-    //       $("#container1").html("<p>hello this is an html<br>paragraph element</p>");
-    //   })
-
-    // $("#menuitem2").click(function(){
-    //     $("#container1").append("this is append");
-    // })
-
-    // $("#menuitem3").click(function(){
-    //   $("#container1").text("this is text");
-    // })
-
-    //   $(".menu2").children().click(function(){
-    //     console.log("click")
-    //     $(this).toggleClass("menuitems")
-    //     $(this).toggleClass("menuitems2")
-    //     console.log("click")
-    //     $("#container2").slideToggle(1000, "swing");
-    //     $("#container2").fadeToggle();
-    //     $("#container2").animate({
-    //       opacity: '0.2',
-    //       height: '60%',
-    //       width: '60%',},2000,function(){
-    //         alert("The paragraph is now hidden");
-    //     });
-    //     })
-    //   $("#menuitem6").click(function(){
-    //       scrollTo(0,5000)
-    //   })
-
-    //   $( ".menu3" ).children().draggable();
 
